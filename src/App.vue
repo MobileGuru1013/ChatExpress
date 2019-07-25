1b09cebce999889fb06dbd3389810bcd7d60fcac
Developer <liuping12@yahoo.com>
2019-07-25 07:24:24 -0700

    Basic Chat Web App using Express.js, Vue.js & Socket.io

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <Chat />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chat from './components/Chat.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chat
+  }
+}
+</script>
+
+<style>
+
+</style>
